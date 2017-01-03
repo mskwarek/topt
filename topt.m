e1c14ed85d20ff12a3c75a852e90d21c88173718
marcin <m.skwarek@stud.elka.pw.edu.pl>
2017-01-03 20:14:39 +0100

    actual

--- topt.m
+++ topt.m
@@ -7,10 +7,13 @@ lambda = 1.55 * 1e-6;
 a = 1e-6;
 
 k = 2* pi./lambda;
-beta = (nc+n)./2 * k;
+
 u = a*sqrt(k^2 * nc^2 - beta^2);
 w = a*sqrt(beta^2 - k^2 * n^2);
+v = a * k * sqrt(nc^2 - n^2);
 
 m = 1
-eq1 = u*besselj(m-1, u)./besselj(m, u) + w* besselh(m-1, w)./besselh(m,w);
-eq2 = w^2 + u^2 - v^2;
+eq1 = u.*besselj(m-1, u)./besselj(m, u) + w.* besselh(m-1, w)./besselh(m,w) == 0;
+eq2 = w^2 + u^2 - v^2 == 0;
+
+solve([eq1, eq2], beta)
